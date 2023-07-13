f27c07fdbb2805c6bacdab3bdc994b26354872ea
irina <obesnyuk@gmail.com>
2023-07-14 01:28:16 +0300

    code changed

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,15 +1,13 @@
 public class BmiService {
-
-    int weightKg = 55;
-    int heightMeter = 168;
-    double d =  (double) heightMeter / 100;
-    double x = (double) d * (double) d;
-    int BMI;
-    int BmiResult;
-
     public int calculate(int BMIresult) {
+        int weightKg = 55;
+        int heightMeter = 168;
+        double d = (double) heightMeter / 100;
+        double x = (double) d * (double) d;
+        int BMI = 1;
+        int BmiResult;
         int result = (int) BMI;
-        result = (int) (weightKg / (double)x);
+        result = (int) (weightKg / (double) x);
         return result;
     }
 }
