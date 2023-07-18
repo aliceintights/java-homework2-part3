226b3444066ad2a4176ae4e85f09b2990d307864
irina <obesnyuk@gmail.com>
2023-07-19 02:10:59 +0300

    parameters

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,9 @@
 public class BmiService {
     public int calculate(int BMIresult) {
         int weightKg = 55;
+        System.out.println(weightKg);
         int heightMeter = 168;
+        System.out.println(heightMeter);
         double d = (double) heightMeter / 100;
         double x = (double) d * (double) d;
         int BMI = 1;
